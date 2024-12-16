22da15defdf7d93eadfc1b4bca93518afad2706b
doyeop-zeticai <doyeop@zetic.ai>
2024-12-16 16:34:45 +0900

    remove 'zetic' prefix

--- feature/entity/inc/box.h
+++ feature/entity/inc/box.h
@@ -5,65 +5,20 @@
 namespace ZeticMLange {
     class Box {
     public:
-        Box() : Box(0, 0, 0, 0) {}
-        Box(float x_min, float y_min, float x_max, float y_max) : x_min{x_min}, y_min{y_min}, x_max{x_max}, y_max{y_max} {}
-        Box(const cv::Rect2f rect) : x_min{rect.x - (rect.width / 2)}, y_min{rect.y - (rect.height / 2)}, x_max{rect.x + (rect.width / 2)}, y_max{rect.y + (rect.height / 2)} {}
-        Box(const Box& other) : x_min{other.x_min}, y_min {other.y_min}, x_max {other.x_max}, y_max{other.y_max} {}
+        Box();
+        Box(float x_min, float y_min, float x_max, float y_max);
+        Box(const cv::Rect2f rect);
+        Box(const Box& other);
 
-        Box operator*(float factor) const {
-            return Box(*this) *= factor;
-        }
-        Box& operator*=(float factor) {
-            x_min *= factor;
-            y_min *= factor;
-            x_max *= factor;
-            y_max *= factor;
-            return *this;
-        }
+        Box operator*(float factor) const;
+        Box& operator*=(float factor);
+        Box operator/(float factor);
+        Box& operator/=(float factor);
 
-        Box operator/(float factor) {
-            return Box(*this) /= factor;
-        }
-        Box& operator/=(float factor) {
-            x_min /= factor;
-            y_min /= factor;
-            x_max /= factor;
-            y_max /= factor;
-            return *this;
-        }
-
-        bool isValid() const {
-            return (x_min < x_max) && (y_min < y_max);
-        }
-
-        Box intersect(const Box& other) const {
-            if (isValid()) {
-                return Box(
-                        std::max(x_min, other.x_min),
-                        std::max(y_min, other.y_min),
-                        std::min(x_max, other.x_max),
-                        std::min(y_max, other.y_max));
-            } else {
-                return Box(0,0,0,0);
-            }
-        }
-
-        float area() const {
-            return (x_max - x_min) * (y_max - y_min);
-        }
-
-        float overlapSimilarity(const Box& other) const {
-            Box intersection = intersect(other);
-            if (!intersection.isValid())
-                return 0.0f;
-            float intersection_area = intersection.area();
-            float denominator = (area()) + (other.area()) - intersection_area;
-            if (denominator > 0.0f) {
-                return intersection_area / denominator;
-            } else {
-                return 0.0f;
-            }
-        }
+        bool isValid() const;
+        Box intersect(const Box& other) const;
+        float area() const;
+        float overlapSimilarity(const Box& other) const;
 
         float x_min;
         float y_min;
